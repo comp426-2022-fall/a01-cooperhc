b14abfc66c79291a0d0da1f2e88b6e0c09b8bbe0
root <root@UNC-R90YDFTH.localdomain>
2022-10-27 16:24:34 -0400

    3

--- server.js
+++ server.js
@@ -5,10 +5,8 @@ const fs = require("fs");
 //const stringify = require("stringify");
 // Require minimist module (make sure you install this one via npm).
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
-var port = require("minimist")(process.argv.slice(2)).port
-if(port == undefined){
-    port = 3000
-}
+const args = require("minimist")(process.argv.slice(2))
+const port = args.port || 3000
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
 
